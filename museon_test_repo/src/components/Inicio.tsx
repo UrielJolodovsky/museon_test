4ec3058ec44fd1967ab14b64febed74a0cf15cc4
tomaslami <101568838+tomaslami@users.noreply.github.com>
2023-05-29 10:44:39 -0300

    add styles and a new index page

---
+++ museon_test_repo/src/components/Inicio.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Inicio = () => {
+
+    const handleClickButton = () => {
+        window.location.href = '/dashboard'
+    }
+
+  return (
+    <div className='start-container'>
+        <div className="container-start-items">
+          <h1 className="start-title">
+           Hola quere dashboard?¡?¡ 
+          </h1>
+          <h2 className='start-subtitle'>
+            Apreta aquí¡!!
+          </h2>
+          <div className="btn-container">
+            <button className='start-btn' onClick={handleClickButton}></button>   
+          </div>  
+        </div>
+    </div>
+  )
+}
+
+export default Inicio
--- museon_test_repo/src/pages/_app.tsx
+++ museon_test_repo/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-// import "../Sass/index.scss";
+import "../Sass/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
