775fd0b2cbdd6b98e308c3ef97a264364560eaf9
tomaslami <tomaslamiguralnik@gmail.com>
2023-05-29 22:26:54 -0300

    login changes

--- museon_test_repo/src/components/Login.tsx
+++ museon_test_repo/src/components/Login.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
@@ -7,11 +8,26 @@ import { FcGoogle } from "react-icons/fc"
 
 
 const Login = () => {
+  //const { data: sessionData, status } = useSession();
+  //const nombre = sessionData?.user ? sessionData?.user?.name : "";
 
   const router = useRouter()
 
-  const handleClickGoogle = () => {
-    router.push('/dashboard')
+  async function LogInGoogle() {
+    try {
+      await signIn("google").then((res) => {
+        console.log(res?.error)
+
+      }).catch((err) => console.log(err))
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const handleIndex = () => {
+    window.location.href = '/'
   }
 
   return (
@@ -21,9 +37,10 @@ const Login = () => {
           <h1 className="login-title">Log In</h1>
           <input type="email" className='inputs' placeholder='email address' />
           <input type="password" className='inputs' placeholder='password' />
-          <button onClick={handleClickGoogle} className='btn'> Log In <AiFillCaretRight /></button>
+          <button className='btn'> Log In <AiFillCaretRight /></button>
           <Link className='link-register' href={"hola"}> Register </Link>
-          <button className="google-btn">
+
+          <button className="google-btn" onClick={LogInGoogle}>
             <FcGoogle className='google-icon' />
             <h3 className="btn-text">Continue with Google</h3>
           </button>
